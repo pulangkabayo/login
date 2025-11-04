ac70c6295591faeb546d87edff2b240c56b18e8c
John Michael <michael.marcos3357@gmail.com>
2025-11-04 09:01:11 +0800

    link all

--- view/php/login.php
+++ view/php/login.php
@@ -3,10 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
     <link rel="stylesheet" href="/login/view/css/login.css">
+    <link rel="stylesheet" href="/login/bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/login/SWAL/sweetalert2.min.css">
+    <script src="/login/jquery/jquery-3.7.1.min.js"></script>
+    <script src="/login/SWAL/sweetalert2.min.js"></script>
 </head>
 <body>
-    
+    <input type="text" class="form-control">
 </body>
 </html>
+<script src="/login/bootstrap-5.3.2-dist/js/bootstrap.bundle.js"></script>
