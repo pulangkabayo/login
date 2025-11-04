454a0c6ca229cc316908afea82d903e033f99aaa
John Michael <michael.marcos3357@gmail.com>
2025-11-05 07:51:09 +0800

    path changes

--- public/login.php
+++ public/login.php
@@ -5,23 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="css/login.css">
-    <link rel="stylesheet" href="/login/bootstrap-5.3.2-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/login/fontawesome-free-6.5.1-web/css/all.min.css">
-    <link rel="stylesheet" href="/login/SWAL/sweetalert2.min.css">
-    <script src="/login/jquery/jquery-3.7.1.min.js"></script>
-    <script src="/login/SWAL/sweetalert2.min.js"></script>
+    <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome-free-6.5.1-web/css/all.min.css">
+    <link rel="stylesheet" href="SWAL/sweetalert2.min.css">
+    <script src="jquery/jquery-3.7.1.min.js"></script>
+    <script src="SWAL/sweetalert2.min.js"></script>
 </head>
 <body>
     <div class="container">
         <div id="loginApp" class="row">
             <form id="loginForm">
-                <h2 id="loginHeader" class="d-flex text-align-center justify-content-center mb-4">Login</h2>
+                <h2 id="loginHeader" class="d-flex text-align-center justify-content-center mb-5">Login</h2>
 
                 <label for="loginUsername">Username</label>
                 <input type="text" id="loginUsername" name="loginUsername" class="form-control mb-3">
 
                 <label for="loginPassword">Password</label>
-                <div class="input-box mb-3 position-relative">
+                <div class="input-box mb-5 position-relative">
                     <input  :type="showPassword ? 'text' : 'password'" 
                         class="form-control" name="loginPassword" id="loginPassword"
                     >
@@ -32,7 +32,7 @@
 
                 <input type="submit" value="Login" class="btn btn-primary form-control mb-3">
                 <a class="d-flex text-align-center justify-content-center mb-3" href="">Forget Password</a>
-                <a class="d-flex text-align-center justify-content-center mb-3" href="">Create Account</a>
+                <a class="d-flex text-align-center justify-content-center mb-3" href="register.php">Create Account</a>
 
             </form>
         </div>
@@ -54,7 +54,7 @@
         });
         loginApp.mount('#loginApp');
     </script>
-    <script src="/login/bootstrap-5.3.2-dist/js/bootstrap.bundle.js"></script>
-    <script src="/login/view/js/login.js"></script>
+    <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.js"></script>
+    <script src="view/js/login.js"></script>
 </body>
 </html>
